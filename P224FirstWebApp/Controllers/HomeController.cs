0ca13f9ffd5c0d3c61feac66f8571b470e104a3c
HamidMammadov <hamidvm@code.edu.az>
2022-03-14 18:12:03 +0400

    First Commit

---
+++ P224FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224FirstWebApp.Controllers
+{
+    public class HomeController
+    {
+        public string Test()
+        {
+            return "Test P224";
+        }
+
+        public string Test2()
+        {
+            return "TEst2";
+        }
+    }
+}
---
+++ P224FirstWebApp/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224FirstWebApp.Controllers
+{
+    public class TestController
+    {
+        public string home()
+        {
+            return "Home";
+        }
+    }
+}
